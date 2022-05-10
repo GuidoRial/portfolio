787632aa513fce776b2710f8c20876191ff9e32c
GuidoRial <rialguido@gmail.com>
2022-05-10 07:11:01 -0300

    deleted sidebar and added flag to header, changed color scheme

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import { flagStyle } from "../../aux";
 import i18next from "i18next";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import { Link } from "react-scroll";
 
-function Navbar({ t, sidebar, setSidebar, showSidebar }) {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const selectSpanish = () => {
-        i18next.changeLanguage("es");
-        setAnchorEl(null);
-    };
-
-    const selectEnglish = () => {
-        i18next.changeLanguage("en");
-        setAnchorEl(null);
-    };
-
-    const matches = useMediaQuery("(min-width:850px)");
-
+function Navbar({ t }) {
     const toggleLanguage = () => {
         i18next.language === "en"
             ? i18next.changeLanguage("es")
@@ -45,52 +21,66 @@ function Navbar({ t, sidebar, setSidebar, showSidebar }) {
             >
                 <h2 className="home">{t("Home")}</h2>
             </Link>
-            {matches ? (
-                <div>
-                    <ul className="link-container">
-                        <Link
-                            style={{ color: "white" }}
-                            to="about"
-                            smooth={true}
-                            duration={1500}
-                        >
-                            <li className="nav-bar-lists">{t("About")}</li>
-                        </Link>
-                        <Link
-                            style={{ color: "white" }}
-                            to="skills"
-                            smooth={true}
-                            duration={1500}
-                        >
-                            <li className="nav-bar-lists">{t("Skills")}</li>
-                        </Link>
-                        <Link
-                            style={{ color: "white" }}
-                            to="projects"
-                            smooth={true}
-                            duration={1500}
-                        >
-                            <li className="nav-bar-lists">{t("Projects")}</li>
-                        </Link>
-                        <Link
-                            style={{ color: "white" }}
-                            to="contact"
-                            smooth={true}
-                            duration={1500}
-                        >
-                            <li className="nav-bar-lists">{t("Contact")}</li>
-                        </Link>
-                        <li
-                            className="nav-bar-lists"
-                            onClick={() => toggleLanguage()}
-                        >
-                            {t("Change_language")}
-                        </li>
-                    </ul>
-                </div>
-            ) : (
-                <i className="fas fa-bars" onClick={showSidebar} />
-            )}
+            <div>
+                <ul className="link-container">
+                    <Link
+                        style={{ color: "white" }}
+                        to="about"
+                        smooth={true}
+                        duration={1500}
+                    >
+                        <li className="nav-bar-lists">{t("About")}</li>
+                    </Link>
+                    <Link
+                        style={{ color: "white" }}
+                        to="skills"
+                        smooth={true}
+                        duration={1500}
+                    >
+                        <li className="nav-bar-lists">{t("Skills")}</li>
+                    </Link>
+                    <Link
+                        style={{ color: "white" }}
+                        to="projects"
+                        smooth={true}
+                        duration={1500}
+                    >
+                        <li className="nav-bar-lists">{t("Projects")}</li>
+                    </Link>
+                    <Link
+                        style={{ color: "white" }}
+                        to="contact"
+                        smooth={true}
+                        duration={1500}
+                    >
+                        <li className="nav-bar-lists">{t("Contact")}</li>
+                    </Link>
+                    <li
+                        className="nav-bar-lists swipeLanguageFlag"
+                        onClick={() => toggleLanguage()}
+                    >
+                        {i18next.language === "en" ? (
+                            <img
+                                className="swipeLanguageFlag"
+                                src={
+                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/800px-Flag_of_the_United_States.svg.png"
+                                }
+                                alt={"country-flag"}
+                                style={flagStyle}
+                            />
+                        ) : (
+                            <img
+                                className="swipeLanguageFlag"
+                                src={
+                                    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png"
+                                }
+                                alt={"country-flag"}
+                                style={flagStyle}
+                            />
+                        )}
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
